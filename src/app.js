0091c9cfe130dd4a0b39aaf66d98e5f76bb7506c
HarshaKy <harshaky6573@gmail.com>
2023-05-03 22:09:25 -0700

    customers and customers/id endpoints

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const db = require('./db/db.json');
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my API!');
+    }
+);
+
+app.get('/customers', (req, res) => {
+    res.json(db.customers);
+    }
+);
+
+app.get('/customers/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const customer = db.customers.find(customer => customer.id === id);
+    if (!customer) {
+        res.status(404).send('The customer with the given ID was not found.');
+        return;
+    }
+    res.json(customer);
+    }
+);
+
+app.get('*', (req, res) => {
+    res.status(404).send('404 Not Found');
+    }
+);
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+    }
+);
